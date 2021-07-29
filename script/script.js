94406e3c299d1667b7492602df20ba5191d4397b
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-07-29 15:54:48 -0300

    macro nutrients demand implemented, next steps, calculate the macronutrients adjuste by technology

--- script/script.js
+++ script/script.js
@@ -10,6 +10,12 @@ let dataLand = {
     ctc: 0,
     PRNT: 70,
 }
+const npkSaca = {
+    nitrogen: [6.2, 6.2],
+    phosphor: [0.6, 0.6],
+    potassium: [5.9, 5.9],
+}
+
 
 function getValue(variable, element) {
     dataLand[variable] = Number(element.value);
@@ -52,6 +58,88 @@ function calcario() {
                 <ul>
                     <li>Demanda por Hectare: <strong>${NC}</strong> T/ha</li>
                     </li>Demandao por Planta: <strong>${NCPlant}</strong> kg/planta</li>
+                </ul>
+            </div>`
+    }
+    else {
+        document.querySelector('.calcario.output-recommendation').innerHTML =
+            `<div class='calcario output-recommendation-result'>
+        <p>Preencha os dados em vermelho</p>
+        </div>`
+    }
+}
+function temperatureAdjuste(npkNeeded) {
+    if (dataLand.temperature < 19) {
+        npkNeeded.nitrogen = [npkNeeded.nitrogen[0] -= 0.15 * npkSaca.nitrogen[0], npkNeeded.nitrogen[1] -= 0.1 * npkSaca.nitrogen[1]]
+    }
+    else if (dataLand.temperature > 22) {
+        npkNeeded.nitrogen = [npkNeeded.nitrogen[0] += 0.15 * npkSaca.nitrogen[0], npkNeeded.nitrogen[1] += 0.2 * npkSaca.nitrogen[1]]
+    }
+    return npkNeeded;
+}
+
+function plantsDensityAdjuste(npkNeeded) {
+    let plantDensity = 10000 / (dataLand.distanceLines * dataLand.distancePlants);
+    if (plantDensity > 6000) {
+        Object.keys(npkNeeded).forEach(item => npkNeeded[item] = [npkNeeded[item][0] -= 0.25 * npkSaca[item][0], npkNeeded[item][1] -= 0.2 * npkSaca[item][1]])
+    }
+    return npkNeeded;
+}
+
+function phosphorAdjuste(npkNeeded) {
+    if (dataLand.phosphor < 10) {
+        npkNeeded.phosphor = [npkNeeded.phosphor[0], npkNeeded.phosphor[1]]
+    }
+    else if (dataLand.phosphor < 20) {
+        npkNeeded.phosphor = [npkNeeded.phosphor[0] -= 0.5 * npkSaca.phosphor[0], npkNeeded.phosphor[1] -= 0.5 * npkSaca.phosphor[1]]
+    }
+    else {
+        npkNeeded.phosphor = [0, 0];
+    }
+    return npkNeeded;
+}
+
+function potassiumAdjuste(npkNeeded) {
+    if (dataLand.potassium < 60) {
+        npkNeeded.potassium = [npkNeeded.potassium[0] += 0.2 * npkSaca.potassium[0], npkNeeded.potassium[1] += 0.3 * npkSaca.potassium[1]]
+    }
+    else if (dataLand.potassium < 110) {
+        npkNeeded.potassium = [npkNeeded.potassium[0], npkNeeded.potassium[1]]
+    }
+    else if (dataLand.potassium < 160) {
+        npkNeeded.potassium = [npkNeeded.potassium[0] -= (2 / 3) * npkSaca.potassium[0], npkNeeded.potassium[1] -= (2 / 3) * npkSaca.potassium[1]]
+    }
+    else {
+        npkNeeded.potassium = [0, 0];
+    }
+    return npkNeeded;
+}
+
+function productivityAdjuste(npkNeeded) {
+    Object.keys(npkNeeded).forEach(item => npkNeeded[item] = [npkNeeded[item][0] * dataLand.productivity, npkNeeded[item][1] * dataLand.productivity]);
+    return npkNeeded;
+}
+
+function macroNutrientsCalculator() {
+    if (entriesTester('macro-nutrients', ['productivity', 'distanceLines', 'distancePlants', 'temperature', 'phosphor', 'potassium'])) {
+        let npkNeeded = {
+            nitrogen: [6.2, 6.2],
+            phosphor: [0.6, 0.6],
+            potassium: [5.9, 5.9],
+        };
+        npkNeeded = temperatureAdjuste(npkNeeded);
+        npkNeeded = plantsDensityAdjuste(npkNeeded)
+        npkNeeded = phosphorAdjuste(npkNeeded);
+        npkNeeded = potassiumAdjuste(npkNeeded);
+        npkNeeded = productivityAdjuste(npkNeeded);
+
+        document.querySelector('.macro-nutrients.output-recommendation').innerHTML =
+            `<div class='macro-nutrients output-recommendation-result'>
+                <p>Correção nescessária:</p>
+                <ul>
+                    <li>Demanda por Nitrogênio: <strong>${npkNeeded.nitrogen[0]} - ${npkNeeded.nitrogen[1]}</strong> kG/ha</li>
+                    <li>Demanda por Fósforo: <strong>${npkNeeded.phosphor[0]} - ${npkNeeded.phosphor[1]}</strong> kG/ha</li>
+                    <li>Demanda por Potássio: <strong>${npkNeeded.potassium[0]} - ${npkNeeded.potassium[1]}</strong> kG/ha</li>
                 </ul>
             </div>`
     }
